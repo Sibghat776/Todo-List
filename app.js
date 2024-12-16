420a51ccf3ee8f4571210be81cadd4480586ad7e
Sibghat776 <ullahsibghat786@gmail.com>
2024-12-16 17:43:41 +0500

    Kuch Functionality Baki Ha

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var myInput = document.querySelector("#myInput");
 var myText = document.querySelector("#text");
 var main = document.querySelector(".main")
 function submitHandler() {
-    console.log("Check Kar Raha Hun");
+    console.log("Check Kar Raha Hun submit handler");
         var myDiv = document.createElement("div")
         myDiv.classList.add("listItem")
         myDiv.innerHTML = `<div class="text">
         <p id="text">${myInput.value}</p>
         </div>
         <div class="btns">
-        <button>Edit</button>
+        <button onclick="editHandler()">Edit</button>
         <button onclick="deleteHandler(this)">Delete</button>
         </div>`
         main.appendChild(myDiv)
@@ -17,7 +17,30 @@ function submitHandler() {
 }
 
 function deleteHandler(btn) {
-    console.log("Check")
-    let listItem = btn.parentElement.parentElement.parentElement.children[2].remove()
+    console.log("Checking delete Handler")
+    let listItem = btn.closest(".listItem")
     listItem.remove()
+    console.log(listItem)
+    myInput.value = ''
+}
+
+function editHandler() {
+    console.log("Checking Ho rha ha edit handler")
+    let myText = document.querySelector("#text")
+    let myInput = document.querySelector("#myInput")
+    console.log(myText.innerHTML)
+    myInput.value = myText.innerHTML
+    myText.innerHTML = ''
+    let input = document.querySelector(".input")
+    input.children[1].innerHTML = "UPDATE"
+    input.children[1].setAttribute("onclick", "updateHandler()")
+}
+function updateHandler() {
+    let myText = document.querySelector("#text")
+    let input = document.querySelector(".input")
+    let myInput = document.querySelector("#myInput")
+    myText.innerHTML = myInput.value
+    myInput.value = ''
+    input.children[1].innerHTML = "SUBMIT"
+    input.children[1].setAttribute("onclick", "submitHandler()")
 }
